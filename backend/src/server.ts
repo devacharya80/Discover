3c220a779e08941fb11f565b13027992de200d5f
Devacharya <dhanudeva80@gmail.com>
2026-09-18 14:56:37 +0530

    feat: complete job ingestion and job discovery flow

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,20 +6,18 @@ import healthRouter from "./routes/health.route.js";
 import authRouter from "./routes/auth.route.js";
 import meRouter from "./routes/me.route.js";
 import userRouter from "./routes/user.route.js";
-import companyRouter from "./routes/company.route.js"
+import companyRouter from "./routes/company.route.js";
+import jobsRouter from "./routes/jobs.route.js";
 
 const app = express();
-
 const PORT = 5000;
 
-app.use(
-  cors({
-    origin: "http://localhost:5173", // URL of your Vite frontend
-    credentials: true,               // Allows cookies & authorization headers
-    methods: ["GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -28,11 +26,9 @@ app.use("/api", healthRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/auth", meRouter);
 app.use("/api/user", userRouter);
-app.use("/api/company",companyRouter)
-
-import {test} from "./test-adzuna.js"
-// await test()
+app.use("/api/company", companyRouter);
+app.use("/api/jobs", jobsRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log("Server running on http://localhost:" + PORT);
 });
