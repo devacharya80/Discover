d9850c688e5f5508bc4c688a39bdff775ac313b0
Devacharya <dhanudeva80@gmail.com>
2026-09-18 15:24:18 +0530

    feat: complete frontend routing and profile workflows

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,32 @@
 import { Routes, Route } from "react-router-dom";
-
 import Home from "./pages/home/Home";
 import CompanyDetails from "./pages/companyPages/CompanyDetails";
 import JobDetails from "./pages/JobDetails";
-
 import ProfileMenu from "./pages/profilePage/Profile";
 import MyProfile from "./pages/profilePage/components/MyProfile";
 import ProfileHome from "./pages/profilePage/components/ProfileHome";
+import Applications from "./pages/profilePage/components/Applications";
+import SavedJobs from "./pages/profilePage/components/SavedJobs";
+import Settings from "./pages/profilePage/components/Settings";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />}>
-
-        {/* Company */}
-        <Route path="company/:companyId" element={<CompanyDetails />}>
-          <Route path="job/:jobId" element={<JobDetails />} />
-        </Route>
-
-        {/* Profile */}
-        <Route path="profile" element={<ProfileMenu />}>
-  <Route index element={<ProfileHome />} />
-  <Route path="me" element={<MyProfile />} />
-</Route>
-
+  return <Routes>
+    <Route path="/" element={<Home />}>
+      <Route path="company/:companyId" element={<CompanyDetails />}>
+        <Route path="job/:jobId" element={<JobDetails />} />
       </Route>
-    </Routes>
-  );
+      <Route path="job/:jobId" element={<JobDetails />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="profile" element={<ProfileMenu />}>
+          <Route index element={<ProfileHome />} />
+          <Route path="me" element={<MyProfile />} />
+          <Route path="applications" element={<Applications />} />
+          <Route path="saved" element={<SavedJobs />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+      </Route>
+    </Route>
+  </Routes>;
 }
-
 export default App;
