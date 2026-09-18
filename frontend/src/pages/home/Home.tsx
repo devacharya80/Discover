7dc45ea22fb64ace3f7767cdf5ade5adc33670a7
Devacharya <dhanudeva80@gmail.com>
2026-09-18 15:25:19 +0530

    feat: integrate job discovery into home map

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,51 +1,10 @@
 import { useState } from "react";
 import { AnimatePresence } from "motion/react";
 import { Outlet } from "react-router-dom";
-
 import MapView from "../../components/Map";
 import Navbar from "../../components/NavBar";
 import Register from "../../components/register/Register";
 import Login from "../../components/login/Login";
-
-type ModalType = "login" | "register" | null;
-
-function Home() {
-  const [activeModal, setActiveModal] = useState<ModalType>(null);
-
-  const closeModal = () => setActiveModal(null);
-
-  return (
-  <main className="relative h-screen w-screen overflow-hidden">
-    {/* Navbar */}
-    <Navbar
-      onOpenLogin={() => setActiveModal("login")}
-      onOpenRegister={() => setActiveModal("register")}
-    />
-
-    {/* Map */}
-    <MapView />
-
-    {/* Child routes */}
-    <Outlet />
-
-    {/* Auth modals */}
-    <AnimatePresence>
-      {activeModal === "register" && (
-        <Register
-          onClose={closeModal}
-          onSwitchToLogin={() => setActiveModal("login")}
-        />
-      )}
-
-      {activeModal === "login" && (
-        <Login
-          onClose={closeModal}
-          onSwitchToRegister={() => setActiveModal("register")}
-        />
-      )}
-    </AnimatePresence>
-  </main>
-);
-}
-
-export default Home;
+import JobDiscoveryPanel from "../../components/JobDiscoveryPanel";
+type ModalType="login"|"register"|null;
+export default function Home(){const[activeModal,setActiveModal]=useState<ModalType>(null);return <main className="relative h-screen w-screen overflow-hidden"><Navbar onOpenLogin={()=>setActiveModal("login")} onOpenRegister={()=>setActiveModal("register")}/><MapView/><JobDiscoveryPanel/><Outlet/><AnimatePresence>{activeModal==="register"&&<Register onClose={()=>setActiveModal(null)} onSwitchToLogin={()=>setActiveModal("login")}/>} {activeModal==="login"&&<Login onClose={()=>setActiveModal(null)} onSwitchToRegister={()=>setActiveModal("register")}/>}</AnimatePresence></main>}
